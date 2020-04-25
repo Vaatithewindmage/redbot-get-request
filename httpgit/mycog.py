cd29b067d18baae60f9c6511661ea11b958a70ef
Vaati <33044807+Vaatithewindmage@users.noreply.github.com>
2020-04-25 03:18:32 -0500

    Update mycog.py

--- httpgit/mycog.py
+++ httpgit/mycog.py
@@ -9,6 +9,4 @@ class Mycog(commands.Cog):
     async def mycom(self, ctx):
         """This does stuff!"""
         # Your code will go here
-        await ctx.send("I can do stuff!")
-        #contents = urllib.request.urlopen("https://tz2.us/temp.php").read()
         await ctx.send(urllib.request.urlopen("https://tz2.us/temp.php").read())
